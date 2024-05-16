5bc88fafb8fdbc795eac79cb263e19a57af768ee
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-05-16 14:57:46 +0530

    Update layout.js

--- src/app/dashBoard/layout.js
+++ src/app/dashBoard/layout.js
@@ -1,5 +1,6 @@
 'use client'
 import DefaultLayout from "@/components/other/DefaultLayout";
+import { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
 export default function RootLayout({ children }) {
   const user = useSelector((state) => state);
@@ -7,6 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body >
+        <Toaster/>
       <DefaultLayout>
        {
         user.login ? children:<div  className=" justify-center items-center flex text-3xl font-semibold h-screen">Login to access the dashboard</div>
