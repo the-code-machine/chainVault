86ff6a534eb9b02a429f230e573ebba7def83589
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-05-16 13:26:01 +0530

    Add files via upload

---
+++ src/controllers/mint.js
@@ -0,0 +1,8 @@
+import Web3 from 'web3';
+import { MintAbi } from '@/contracts/ABIs/mint';
+import { DocumentAddress } from '@/contracts/ABIs/mint';
+import { alchemyUrl } from '@/contracts/ABIs/mint';
+
+
+
+
